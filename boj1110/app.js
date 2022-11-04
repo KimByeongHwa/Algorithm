99d3a97da6cbee6d2ab02697b0edfe1a53937e71
KimByeongHwa <iambyeonghwa@gmail.com>
2022-11-05 04:35:33 +0900

    221104 boj1110 해결(수정 필요)

--- boj1110/app.js
+++ boj1110/app.js
@@ -1,33 +1,36 @@
 let input = require('fs').readFileSync('input.txt').toString();
 
-let N = +input;
+let firstN = parseInt(input);
 let sum;
-let result;
+let newN;
+let a;
+let b;
+let b2;
 let cycle = 0;
 
-while(result == N){
-    if(N<10){
-        sum = N;
-        result = sum*2;
-        cycle++;
-        N = result;
-    }
-    else{
-        let a = parseInt(N/10);
-        let b = N%10;
-        sum = a+b;
-        if(sum<10){
-            result = b + sum;
-            cycle++;
-            N = result;
-        }
-        else{
-            let sumA = parseInt(sum/10);
-            let sumB = sum%10;
-            result = b + sumB;
-            cycle++;
-            N = result;
-        }
+N = firstN;
+a = parseInt(N/10);     // N=26
+b = parseInt(N%10);
+sum = parseInt(a+b);
+b2 = parseInt(sum%10);
+newN = parseInt(b*10 + b2);    // newN = 68
+cycle++;
+// console.log(N, firstN);
+if (newN === N) {
+    console.log(cycle);
+    return cycle;
+}
+while(true){
+    N = newN;   // N = 68
+    // console.log("while", N, newN);
+    a = parseInt(N/10);
+    b = parseInt(N%10);
+    sum = parseInt(a+b);
+    b2 = parseInt(sum%10);
+    newN = parseInt(b*10 + b2); // newN = 84
+    cycle++;
+    if (newN === firstN) {
+        console.log(cycle);
+        return cycle;
     }
 }
-console.log(cycle);
